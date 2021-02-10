e8ff6ad22c7839e0b8b0895d33891c8ac0945a7a
Theo Sannie <vipers12000@gmail.com>
2021-02-10 16:54:36 +0100

    [+] add multi command with ';'

--- minishell_basic/srcs/start_sh.c
+++ minishell_basic/srcs/start_sh.c
@@ -5,38 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsannie <tsannie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/10 13:13:37 by phbarrad          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/02/10 14:29:49 by phbarrad         ###   ########.fr       */
-=======
-/*   Updated: 2021/02/10 13:34:06 by tsannie          ###   ########.fr       */
->>>>>>> fa5cd6f8ca3ca5d58a557da7b7c7fdb3b9b082fe
+/*   Created: 2021/02/10 16:12:51 by tsannie           #+#    #+#             */
+/*   Updated: 2021/02/10 16:53:16 by tsannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
 
 #include "../includes/minish.h"
 
 int ft_strcmpp(char *s1, char *s2)
 {
-    int i;
+	int i;
 	int j;
-	
+
 	i = 0;
 	j = 0;
 	while (s1[i] == ' ' || s1[i] == '\t')
 		i++;
-    while (s2[j] && s1[i] != ';') // ; = separe les commandes
-    {
-        if (s1[i] > s2[j])
-            return (1);
-        else if (s1[i] < s2[j])
-            return (-1);
-        j++;
+	while (s2[j] && s1[i] != ';') // ; = separe les commandes
+	{
+		if (s1[i] > s2[j])
+			return (1);
+		else if (s1[i] < s2[j])
+			return (-1);
+		j++;
 		i++;
-    }
-    if (s1[i] == ' ' || s1[i] == '\t' || s1[i] == '\0' || s1[i] == ';')
-        return (0);
-    return (1);
+	}
+	if (s1[i] == ' ' || s1[i] == '\t' || s1[i] == '\0' || s1[i] == ';')
+		return (0);
+	return (1);
 }
 
 int check_cmd(char *str)
@@ -48,7 +46,7 @@ int check_cmd(char *str)
 	{
 		if (str[i] != ' ' && str[i] != '\t')
 			return (0);
-		i++;	
+		i++;
 	}
 	return (1);
 }
@@ -71,38 +69,40 @@ void ft_putstr_not_found(char *str)
 
 char *get_val(void)
 {
-    char **line;
+	char **line;
 
 	line = malloc(sizeof(char *) * 1);
 	get_next_line(0, line);
-    return (*line);
+	return (*line);
 }
 
 void	start_shell(int ac, char **av, char **envp, t_set *set)
 {
+	char	**res;
+
 	set->str = get_val();
-<<<<<<< HEAD
-=======
-
-	//ft_putstr_fd(set->str, 1);
-
->>>>>>> fa5cd6f8ca3ca5d58a557da7b7c7fdb3b9b082fe
-	if (ft_strcmpp(set->str, "exit") == 0)
-        exit(0);
-	else if (ft_strcmpp(set->str, "echo") == 0)
-        ft_echo(set);
-	else if (ft_strcmpp(set->str, "cd") == 0)
-        ft_cd(set);
-	else if (ft_strcmpp(set->str, "pwd") == 0)
-		ft_pwd(set);
-	else if (ft_strcmpp(set->str, "export") == 0)
-		ft_export(set);
-	else if (ft_strcmpp(set->str, "unset") == 0)
-		ft_unset(set, envp);
-	else if (ft_strcmpp(set->str, "env") == 0)
-		ft_env(set, envp);
-	else if (ft_strcmpp(set->str, "clear") == 0)
-		ft_putstr_fd("\033[H\033[2J", 1);
-    else if (ft_strlen(set->str) != 0 && check_cmd(set->str) == 0)
-		ft_putstr_not_found(set->str);
+	res = ft_split(set->str, ';'); // faut free res a un moment mais je trouve pas quand
+	while (*res)
+	{
+		set->str = *res;
+		if (ft_strcmpp(set->str, "exit") == 0)
+			exit(0);
+		else if (ft_strcmpp(set->str, "echo") == 0)
+			ft_echo(set);
+		else if (ft_strcmpp(set->str, "cd") == 0)
+			ft_cd(set);
+		else if (ft_strcmpp(set->str, "pwd") == 0)
+			ft_pwd(set);
+		else if (ft_strcmpp(set->str, "export") == 0)
+			ft_export(set);
+		else if (ft_strcmpp(set->str, "unset") == 0)
+			ft_unset(set, envp);
+		else if (ft_strcmpp(set->str, "env") == 0)
+			ft_env(set, envp);
+		else if (ft_strcmpp(set->str, "clear") == 0)
+			ft_putstr_fd("\033[H\033[2J", 1);
+		else if (ft_strlen(set->str) != 0 && check_cmd(set->str) == 0)
+			ft_putstr_not_found(set->str);
+		res++;
+	}
 }
