286505a1e3ba96f91fb7bf49f76a2a1e3925aab6
Philippe BARRADAS <phbarrad@student.42.fr>
2021-02-10 11:35:25 +0100

    dossier de base et bonus

--- minishell_b/includes/minish.h
+++ minishell_b/includes/minish.h
@@ -1,23 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   minish.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: phbarrad <phbarrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/17 14:26:03 by phbarrad          #+#    #+#             */
-/*   Updated: 2021/02/10 11:07:05 by phbarrad         ###   ########.fr       */
+/*   Created: 2021/02/10 11:07:34 by phbarrad          #+#    #+#             */
+/*   Updated: 2021/02/10 11:33:05 by phbarrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/libft.h"
 
-size_t				ft_strlen(const char *str)
-{
-	size_t	i;
+#ifndef MINISH_H
+# define MINISH_H
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
+# include "../../libft/libft.h"
+
+#endif
