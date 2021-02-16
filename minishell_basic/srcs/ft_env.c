c517ac3ab3a210c273a43e96cd22c7a58aa58424
philippebarradas <philippe.lb.barradas@gmail.com>
2021-02-16 10:27:58 +0100

    [+] Exit 2/3

--- minishell_basic/srcs/ft_env.c
+++ minishell_basic/srcs/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: phbarrad <phbarrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 13:18:30 by phbarrad          #+#    #+#             */
-/*   Updated: 2021/02/15 09:13:09 by phbarrad         ###   ########.fr       */
+/*   Updated: 2021/02/15 16:20:53 by phbarrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int ft_env(t_set *set, char **envp)
     int i;
 	
 	i = -1;
-    while (envp[++i] != NULL)
-        printf("%s\n", envp[i]);
+    while (set->hide_envp[++i] != NULL)
+        printf("%s\n", set->hide_envp[i]);
     return (0);
 }
